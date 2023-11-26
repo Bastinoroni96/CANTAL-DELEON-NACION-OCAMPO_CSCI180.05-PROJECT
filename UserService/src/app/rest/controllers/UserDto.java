ca4332db7d299842391710a987bcd8b1d0dbd6cc
Bastinoroni96 <gsbasti96@gmail.com>
2023-11-26 11:06:25 +0800

    Committing UserService

---
+++ UserService/src/app/rest/controllers/UserDto.java
@@ -0,0 +1,36 @@
+package app.rest.controllers;
+
+public class UserDto {
+	
+	private String username;
+	private String password;
+	private String name;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDto [username=" + username + ", password=" + password + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
